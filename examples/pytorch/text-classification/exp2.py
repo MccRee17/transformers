4bd6b9a4463596ebc558d428e9a1beee1d72b5fc
Dacheng Li <dal177@ucsd.edu>
2022-08-07 21:30:58 +0000

    update exp2

--- examples/pytorch/text-classification/exp2.py
+++ examples/pytorch/text-classification/exp2.py
@@ -39,38 +39,39 @@ with open(log_path, "a") as f:
 num_devices = torch.cuda.device_count()
 
 def HPO_S0():
-    lr_list = [5e-6,1e-6]
-    bs_list = [384, 256]
+    lr_list = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4]
+    bs_list = [256, 128, 64, 32, 16]
+    epoch_list = [50, 80, 100, 200, 3, 10, 30]
     best = None
     best_metric = 0
 
     for lr in lr_list:
         for bs in bs_list:
-            output_dir = os.path.join(base_dir, "S0" ,str(lr), str(bs))
-            result_path = os.path.join(output_dir, "eval_results.json")
-            cmd = f"python run_glue_scratch.py --model_name_or_path bert-base-uncased \
-                  --task_name {task_name} --warmup_ratio 0.2\
-                  --do_train --do_eval --max_seq_length 128 --per_device_train_batch_size {str(bs//num_devices)} --learning_rate {str(lr)} \
-                  --num_train_epochs 10 --act quad --softmax_act 2relu --output_dir {output_dir} --overwrite_output_dir"
+            for epoch in epoch_list:
+                output_dir = os.path.join(base_dir, "HPO_S0" ,str(lr), str(bs), str(epoch))
+                result_path = os.path.join(output_dir, "eval_results.json")
+                cmd = f"python run_glue_scratch.py --model_name_or_path {model_path} \
+                      --task_name {task_name} \
+                      --do_train --do_eval --max_seq_length 128 --warmup_ratio 0.2 --per_device_train_batch_size {str(bs//num_devices)} --learning_rate {str(lr)} \
+                      --num_train_epochs {epoch} --act quad --softmax_act 2relu --output_dir {output_dir} --overwrite_output_dir"
 
-            subprocess.run(cmd, shell=True)
-            result = json.load(open(result_path))
-            metric = float(result[metric_name])
-            if metric > best_metric:
-                best = (lr, bs)
-                best_metric = metric
-            with open(log_path, "a") as f:
-                f.write(f"pretrain S0 with lr {str(lr)} bs {str(bs)}, acc: {metric} \n")
+                subprocess.run(cmd, shell=True)
+                result = json.load(open(result_path))
+                metric = float(result[metric_name])
+                if metric > best_metric:
+                    best = (lr, bs, epoch)
+                    best_metric = metric
+                with open(log_path, "a") as f:
+                    f.write(f"fine-tuned S0 with lr {str(lr)} bs {str(bs)} epoch {epoch}, acc: {metric} \n")
 
-    best_lr, best_bs = best
+    best_lr, best_bs, best_epoch = best
     with open(log_path, "a") as f:
-        f.write(f"best S0 with lr {best_lr} bs {best_bs}, acc: {best_metric} \n")
-
+        f.write(f"best S0 with lr {best_lr} bs {best_bs} epoch {best_epoch}, acc: {best_metric} \n")
 
 def HPO_S1():
     lr_list = [1e-6, 5e-6, 1e-5, 5e-5, 1e-4, 5e-4]
     bs_list = [256, 128, 64, 32, 16]
-    epoch_list = [50, 80, 3, 10, 30]
+    epoch_list = [3, 10, 30, 50, 80, 100, 200]
     #lr_list = [1e-4]
     #bs_list = [8]
     best = None
